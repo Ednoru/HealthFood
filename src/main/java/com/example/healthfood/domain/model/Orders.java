82d504e5d9c543d8fca1f32c06c997d3ca8db9e2
XLianLZ <Lianbox7304@gmail,com>
2024-04-26 19:07:47 -0500

    Orders API

--- src/main/java/com/example/healthfood/domain/model/Orders.java
+++ src/main/java/com/example/healthfood/domain/model/Orders.java
@@ -1,4 +1,40 @@
 package com.example.healthfood.domain.model;
 
+import com.example.healthfood.domain.model.User;
+import com.example.healthfood.domain.model.Food;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Time;
+import java.util.Date;
+
+
+@Setter
+@Getter
+@Entity
+@Table(name = "Orders")
+@Data
 public class Orders {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private Date delivery_date;
+
+    @Column(nullable = false)
+    private Time delivery_time;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "food_id")
+    private Food food;
 }
